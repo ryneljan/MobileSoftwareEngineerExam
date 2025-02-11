61b159438ab15275406422ebf5a7ca8be785d068
rynel <ranajirune@gmail.com>
2025-02-11 15:17:30 +0800

    Added validation form for full name and email

--- app/src/main/java/com/raineru/msee/MainActivity.kt
+++ app/src/main/java/com/raineru/msee/MainActivity.kt
@@ -3,25 +3,33 @@ package com.raineru.msee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.raineru.msee.ui.theme.MobileSoftwareEngineerExamTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             MobileSoftwareEngineerExamTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var fullName by remember { mutableStateOf("") }
+        var isFullNameValid by remember { mutableStateOf(false) }
+
+        OutlinedTextField(
+            value = fullName,
+            onValueChange = {
+                fullName = it
+                isFullNameValid = it.isValidFullName()
+            },
+            label = { Text("Full Name") },
+            isError = isFullNameValid,
+            textStyle = TextStyle(fontSize = 16.sp),
+            supportingText = {
+                if (!isFullNameValid) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = "Invalid Full Name",
+                        color = Color.Red
+                    )
+                }
+            }
+        )
+
+        var email by remember { mutableStateOf("") }
+        var isEmailValid by remember { mutableStateOf(false) }
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+                isEmailValid = it.isValidEmail()
+            },
+            label = { Text("Email") },
+            isError = isEmailValid,
+            textStyle = TextStyle(fontSize = 16.sp),
+            supportingText = {
+                if (!isEmailValid) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = "Invalid Email",
+                        color = Color.Red
+                    )
+                }
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     MobileSoftwareEngineerExamTheme {
-        Greeting("Android")
+        HomeScreen()
     }
+}
+
+fun String.isValidFullName(): Boolean {
+    val trimmedString = this.trim()
+    val regex = Regex("^[a-zA-Z]+( [a-zA-Z]+|[.,])*$")
+    return regex.matches(trimmedString)
+}
+
+fun String.isValidEmail(): Boolean {
+    val emailRegex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+    return emailRegex.matches(this)
 }
