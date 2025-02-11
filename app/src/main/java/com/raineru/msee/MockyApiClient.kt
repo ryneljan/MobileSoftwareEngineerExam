5f19d664b5890ec6b61725f0166d19190dc0e835
rynel <ranajirune@gmail.com>
2025-02-11 18:04:00 +0800

    Added safeguards when the response from the api is not OK

--- app/src/main/java/com/raineru/msee/MockyApiClient.kt
+++ app/src/main/java/com/raineru/msee/MockyApiClient.kt
@@ -3,6 +3,7 @@ package com.raineru.msee
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
 import kotlinx.io.IOException
 
 class MockyApiClient(
@@ -11,10 +12,14 @@ class MockyApiClient(
 
     suspend fun submitForm(mockyId: String): ApiResponse {
         try {
-            val response: FormSubmissionResponse = httpClient.get(
+            val response: HttpResponse = httpClient.get(
                 "https://run.mocky.io/v3/$mockyId"
-            ).body()
-            return ApiResponse.Success(response)
+            )
+            return if (response.status == io.ktor.http.HttpStatusCode.OK) {
+                ApiResponse.Success(response.body())
+            } else {
+                ApiResponse.Error("Error: ${response.status}")
+            }
         } catch (e: IOException) {
             return ApiResponse.Error(e.toString())
         }
