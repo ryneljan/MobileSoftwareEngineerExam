2856744d936abe0477d2563673d7495967fc752d
rynel <ranajirune@gmail.com>
2025-02-11 21:17:40 +0800

    Reverted api client to return raw String instead of class

--- app/src/main/java/com/raineru/msee/MockyApiClient.kt
+++ app/src/main/java/com/raineru/msee/MockyApiClient.kt
@@ -1,27 +1,23 @@
 package com.raineru.msee
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import kotlinx.io.IOException
 
 class MockyApiClient(
     private val httpClient: HttpClient
 ) {
 
-    suspend fun submitForm(mockyId: String): ApiResponse {
+    suspend fun submitForm(mockyId: String): String {
         try {
             val response: HttpResponse = httpClient.get(
                 "https://run.mocky.io/v3/$mockyId"
             )
-            return if (response.status == io.ktor.http.HttpStatusCode.OK) {
-                ApiResponse.Success(response.body())
-            } else {
-                ApiResponse.Error("Error: ${response.status}")
-            }
+            return response.bodyAsText()
         } catch (e: IOException) {
-            return ApiResponse.Error(e.toString())
+            return e.toString()
         }
     }
 }
